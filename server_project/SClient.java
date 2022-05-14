ca6fa7909044055826d1803eb92fc2a5f9c2a522
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2022-05-14 20:00:44 +0300

    Update SClient.java

--- server_project/SClient.java
+++ server_project/SClient.java
@@ -7,163 +7,231 @@ package server;
 
 import message.Message;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.concurrent.Semaphore;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import static server.Server.Clients;
+import javax.swing.DefaultListModel;
 
 /**
  *
  * @author 
  */
-public class SClient {
+class ServerThread extends Thread {
 
-    int id;
-    public String name = "NoName";
-    Socket soket;
-    ObjectOutputStream sOutput;
-    ObjectInputStream sInput;
-    //clientten gelenleri dinleme threadi
-    Listen listenThread;
-    //karsilikli mesajlasma icin eslestirme ve mesaj alisverisi threadi
-    PairingThread2 pairThread;
+    public void run() {
+        //server kapanana kadar dinle
+        while (!Server.serverSocket.isClosed()) {
+            try {
+                Server.Display("Client Bekleniyor...");
+                // clienti bekleyen satır
+                //bir client gelene kadar bekler
+                Socket clientSocket = Server.serverSocket.accept();
+                //client gelirse bu satıra geçer
+                Server.Display("Client Geldi...");
+                //gelen client soketinden bir sclient nesnesi oluştur
+                //bir adet id de kendimiz verdik
+                SClient nclient = new SClient(clientSocket, Server.IdClient);
+                Server.IdClient++;
+                //clienti listeye ekle.
+                Server.Clients.add(nclient);
+                //client mesaj dinlemesini başlat
+                nclient.listenThread.start();
 
-    public SClient(Socket gelenSoket, int id) {
-        this.soket = gelenSoket;
-        this.id = id;
+            } catch (IOException ex) {
+                Logger.getLogger(ServerThread.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }
+}
+
+public class Server {
+
+    //server soketi eklemeliyiz
+    public static ServerSocket serverSocket;
+    public static int IdClient = 0;
+    // Serverın dileyeceği port
+    public static int port = 0;
+    //Serverı sürekli dinlemede tutacak thread nesnesi
+    public static ServerThread runThread;
+
+    public static ArrayList<SClient> Clients = new ArrayList<>();
+
+    // başlaşmak için sadece port numarası veriyoruz
+    public static void Start(int openport) {
         try {
-            this.sOutput = new ObjectOutputStream(this.soket.getOutputStream());
-            this.sInput = new ObjectInputStream(this.soket.getInputStream());
+            Server.port = openport;
+            Server.serverSocket = new ServerSocket(Server.port);
+
+            Server.runThread = new ServerThread();
+            Server.runThread.start();
+
+        } catch (IOException ex) {
+            Logger.getLogger(Server.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    public static void Display(String msg) {
+        System.out.println(msg);
+    }
+
+    // serverdan clientlara mesaj gönderme
+    //clienti alıyor ve mesaj yolluyor
+    public static void Send(SClient cl, Message msg) {
+        try {
+            cl.sOutput.writeObject(msg);
         } catch (IOException ex) {
             Logger.getLogger(SClient.class.getName()).log(Level.SEVERE, null, ex);
         }
-        //thread nesneleri
-        this.listenThread = new Listen(this);
     }
 
-    //client mesaj gönderme
-    public void Send(Message message) {
-        try {
-            this.sOutput.writeObject(message);
-        } catch (IOException ex) {
-            Logger.getLogger(SClient.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
+    //Yeni Grup olusturmak icin
+    public static void CreateGrup(Message msg) {
+        String[] parts = msg.content.toString().split("_");
+        String grup_adi = parts[0];
+        String geri_kalan = parts[1];
+        String[] parts2 = geri_kalan.split("-");
+        msg.content = grup_adi + "_" + geri_kalan;
 
-    //client dinleme threadi
-    //her clientin ayrı bir dinleme threadi var
-    class Listen extends Thread {
-
-        SClient TheClient;
-        Message msg;
-
-        //thread nesne alması için yapıcı metod
-        Listen(SClient TheClient) {
-            this.TheClient = TheClient;
-        }
-
-        public void run() {
-            //client bağlı olduğu sürece dönsün
-            while (TheClient.soket.isConnected()) {
-                try {
-                    //mesajı bekleyen kod satırı               
-                    Message received = (Message) (TheClient.sInput.readObject());
-                    //mesaj gelirse bu satıra geçer
-                    //mesaj tipine göre işlemlere ayır
-                    switch (received.type) {
-                        case Name:
-                            //kisinin isim bilgisini aldıktan sonra baglanti kurar
-                            TheClient.name = received.content.toString();
-                            Thread.sleep(500);
-                            Server.Send(TheClient, received);
-                            Thread.sleep(500);
-                            Server.BaglantiKur(received);
-                            break;
-
-                        case kisiBul:
-                            //karsilikli mesajlasilirken karsiki kisiyle konusmak icin thread baslatir
-                            TheClient.pairThread = new SClient.PairingThread2(received);
-                            pairThread.start();
-                            break;
-
-                        case grupKisiBul:
-                            //Secili kisilerle grup olusturur
-                            Server.CreateGrup(received);
-                            break;
-
-                        case icerik2:
-                            //karsi clienta mesaji iletir
-                            Server.KarsiyaGonder(received);
-                            break;
-
-                        case baglantiKopar:
-                            //Clientlardan biri konusmadan ciktiktan sonra karsiki clienta bunun bilgisi gider 
-                            //konusma sonlanir
-                            SClient c = Server.ClientBul(received.content.toString());
-                            Server.Send(c, received);
-                            Thread.sleep(100);
-                            break;
-
-                        case baglantiKopar2:
-                            //clientlardan biri konusmadan cikinca o konusmayi takip eden threadi durdurur
-                            Thread.sleep(100);
-                            TheClient.pairThread.stop();
-                            break;
-
-                        case grupUsers:
-                            //grup olusturmak icin online kullanicilar bilgisini tasir
-                            Server.BaglantiKur2(received);
-                            Thread.sleep(100);
-                            break;
-
-                        case icerikGrup:
-                            //gruptaki tum uyelere mesaji iletir
-                            Server.tumUyelereGonder(received);
-                            break;
-
-                        case dosya1:
-                            //gruptaki kullanicilara dosyayi gonderir
-                            Server.dosyaGonder(received);
-                            break;
-                    }
-                } catch (IOException ex) {
-                    Logger.getLogger(SClient.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (ClassNotFoundException ex) {
-                    Logger.getLogger(SClient.class.getName()).log(Level.SEVERE, null, ex);
-                } catch (InterruptedException ex) {
-                    Logger.getLogger(SClient.class.getName()).log(Level.SEVERE, null, ex);
+        //secili kisilere grup olusturma mesajini yollar
+        for (SClient c : Clients) {
+            for (String p : parts2) {
+                if (c.name.equals(p)) {
+                    Server.Send(c, msg);
                 }
             }
         }
     }
 
-    class PairingThread2 extends Thread {
+    //Yeni bireysel mesajlasma olusturmak icin
+    public static void CreateBireyselMesajlasma(Message msg) throws InterruptedException {
+        String[] parts = msg.content.toString().split("-");
+        String kisi_adi = parts[0];
+        String geri_kalan = parts[1];
+        msg.content = geri_kalan;
 
-        //karsilikli mesajlasmayi saglamak icin baslatilan thread
-        //her mesajlasma icin bir thread
-        Message msg;
-
-        PairingThread2(Message msg) {
-            this.msg = msg;
+        //secili kisiye yeni jframe olusturmak icin
+        for (SClient c : Clients) {
+            if (c.name.equals(kisi_adi)) {
+                Server.Send(c, msg);
+                Thread.sleep(100);
+            }
         }
+    }
 
-        public void run() {
-            String[] parts = msg.content.toString().split("-");
-            String kisi_adi = parts[0];
-            String geri_kalan = parts[1];
-            msg.content = msg.content.toString();
+    //Bireysel mesajlasmalarda yazilan mesaji karsiya gonderir
+    public static void KarsiyaGonder(Message msg) {
+        String[] parts = msg.content.toString().split("_");
+        String kisi = parts[0];
+        String mesaj = parts[1];
+        msg.content = kisi + "_" + mesaj;
 
-            //secili kisiye yeni jframe olusturmak icin
-            for (SClient c : Clients) {
-                if (c.name.equals(kisi_adi)) {
+        //karsiki clienta mesaj icerigini gondermek icin 
+        for (SClient c : Clients) {
+            if (c.name.equals(kisi)) {
+                Server.Send(c, msg);
+            }
+        }
+    }
+
+    //Grup mesajlasmalarinda grupta yazilan mesajlari tum kullanicilara gonderir
+    public static void tumUyelereGonder(Message msg) {
+        String[] parts = msg.content.toString().split("_");
+        String kisiler = parts[0];
+        String mesaj = parts[1];
+        msg.content = mesaj;
+        String[] parts2 = kisiler.split("-");
+        //tum grup uyelerine mesaj icerigini gonderir
+        for (SClient c : Clients) {
+            for (String p : parts2) {
+                if (c.name.equals(p)) {
                     Server.Send(c, msg);
-                    try {
-                        Thread.sleep(100);
-                    } catch (InterruptedException ex) {
-                        Logger.getLogger(SClient.class.getName()).log(Level.SEVERE, null, ex);
-                    }
+                }
+            }
+        }
+    }
+
+    public static void dosyaGonder(Message msg) throws InterruptedException {
+        //Tum grup uyelerine dosyayi gonderir
+        Thread.sleep(100);
+        String[] parts = msg.content.toString().split("&");
+        String kisiler = parts[0];
+        String fileName_Content = parts[1];
+        msg.content = fileName_Content;
+        String[] parts2 = kisiler.split("-");
+
+        for (SClient c : Clients) {
+            for (String p : parts2) {
+                if (c.name.equals(p)) {
+                    Server.Send(c, msg);
+                }
+            }
+        }
+    }
+
+    public static void dosyaGonder2(Message msg) throws InterruptedException {
+        //Karsiki clienta dosyayi gonderir
+        Thread.sleep(100);
+        String[] parts = msg.content.toString().split("&");
+        String kisi = parts[0];
+        String fileName_Content = parts[1];
+        msg.content = fileName_Content;
+
+        for (SClient c : Clients) {
+            if (c.name.equals(kisi)) {
+                Server.Send(c, msg);
+            }
+        }
+    }
+
+    public static SClient ClientBul(String s) {
+
+        for (SClient c : Clients) {
+            if (c.name.equals(s)) {
+                //Server.Send(c, msg2);
+                return c;
+            }
+        }
+        return null;
+    }
+
+    public static void BaglantiKur(Message msg) throws InterruptedException {
+        //Her yeni baglanan kullaniciyi cevrimici kullanicilar listesine ekler 
+        //Bunu tüm clientlara yollar
+        Thread.sleep(200);
+        if (Server.Clients.size() > 0) {
+            DefaultListModel userList = new DefaultListModel();
+            for (SClient client : Clients) {
+                userList.addElement(client.name);
+            }
+
+            Message msg2 = new Message(Message.Message_Type.Connected);
+            msg2.content = userList;
+
+            for (SClient c : Clients) {
+                Server.Send(c, msg2);
+            }
+        }
+    }
+
+    public static void BaglantiKur2(Message msg) throws InterruptedException {
+        //Grup kurmak isteyen clienta tüm çevrimiçi kullanicilarin listesini yollar
+        Thread.sleep(100);
+        if (Server.Clients.size() > 0) {
+            DefaultListModel userList = new DefaultListModel();
+            for (SClient client : Clients) {
+                userList.addElement(client.name);
+            }
+
+            Message msg2 = new Message(Message.Message_Type.grupUsers);
+            msg2.content = userList;
+
+            for (SClient c : Clients) {
+                if (c.name.equals(msg.content.toString())) {
+                    Server.Send(c, msg2);
+                    break;
                 }
             }
         }
