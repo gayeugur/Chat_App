271ceeb861bb0b3b89dc41b867e60e607678c5f7
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2022-05-15 18:10:04 +0300

    Update Client.java

--- client_project/Client.java
+++ client_project/Client.java
@@ -7,27 +7,28 @@ package client;
 
 /**
  *
- * @author 
+ * @author
  */
 import static client.Client.sInput;
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
+import java.util.Base64;
+import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import message.Message;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.util.Base64;
 import javax.swing.DefaultListModel;
 import javax.swing.JFileChooser;
-import message.anasayfa;
+import message.Message;
 import static message.Room.ThisSohbet;
+import message.anasayfa;
 
 /**
  *
- * @author busra
+ * @author gayeu
  */
 class Listen extends Thread {
 
@@ -73,11 +74,7 @@ class Listen extends Thread {
                         message.Room.ThisSohbet.setVisible(true);
                         message.Room.ThisSohbet.list_mess.setText(mesajakisi2);
                         break;
-//
-//                    case durum:
-//                        Thread.sleep(500);
-//                        sohbet.durum = (boolean) received.content;
-//                        break;
+
                     case baglantiKopar:
                         //karsi client sohbet kutusunu kapatirsa burada da mesajlasma sonlandirilir 
                         ThisSohbet.setVisible(false);
@@ -88,7 +85,7 @@ class Listen extends Thread {
                     case grupUsers:
                         //gruptaki kisiler bilgisi gonderilir grup uyelerine
                         Thread.sleep(100);
-                     //   grupOlustur.ThisGrupOlustur.users.setModel((DefaultListModel) received.content);
+                       //    grupOlustur.ThisGrupOlustur.users.setModel((DefaultListModel) received.content);
                         break;
 
                     case grupKisiBul:
@@ -96,29 +93,26 @@ class Listen extends Thread {
                         if (message.anasayfa.ThisAnasayfa.isVisible()) {
                             message.anasayfa.ThisAnasayfa.setVisible(false);
                         }
-                        new message.GrupOlusturs(received.content.toString(), 1).setVisible(true);
+                        
+                        new message.Group(received.content.toString()).setVisible(true);
                         break;
 
                     case kisiBul:
                         //bireysel mesajlasmada biri sohbeti baslatinca digerinin de sayfasinda o sohbet penceresi acilir
                         if (message.anasayfa.ThisAnasayfa.isVisible()) {
                             message.anasayfa.ThisAnasayfa.setVisible(false);
-                        }      
+                        }
                         String[] dizi = received.content.toString().split("-");
                         String kisi_ad = dizi[0];
-                        String kisi_ad2 = dizi[1];
-                        System.out.println("ikinic" + kisi_ad2);     
-                        
-                        new message.Room(kisi_ad2,kisi_ad2).setVisible(true);
-                        break;
-                        
-             
+                        Client.kisi_ad2 = dizi[1];
+                        System.out.println("ikinic" + Client.kisi_ad2);
 
-                        
+                        new message.Room().setVisible(true);
+                        break;
 
                     case icerikGrup:
                         //gruptaki mesaj iceriklerini gunceller
-                        message.GrupOlusturs.ThisGrupSohbet.grup_mesaj_akisi.setText(received.content.toString());
+                        message.Room.ThisSohbet.list_mess.setText(received.content.toString());
                         break;
 
                     case dosya1:
@@ -154,6 +148,7 @@ class Listen extends Thread {
 
 public class Client {
 
+    public static String kisi_ad2;
     //her clientın bir soketi olmalı
     public static Socket socket;
 
