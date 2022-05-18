3fdd026a2ab5f182ea9c7976fa261213ee389735
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2022-05-19 00:08:27 +0300

    Update grup.java

--- message/grup.java
+++ message/grup.java
@@ -3,14 +3,13 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package message;
+package application;
 
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Base64;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
@@ -19,7 +18,7 @@ import javax.swing.JOptionPane;
 
 /**
  *
- * @author busra
+ * @author gayeu
  */
 public class Group extends javax.swing.JFrame {
 
@@ -36,36 +35,40 @@ public class Group extends javax.swing.JFrame {
     public Group() {
         initComponents();
         ThisGrupSohbet = this;
-        lbl_grupName.setText(anasayfa.groupName);
+        //  lbl_grupName.setText(anasayfa.groupName);
     }
 
-    public Group(String s) {
+    public Group(String s) throws InterruptedException {
         initComponents();
         ThisGrupSohbet = this;
-        //   grup_mesaj_akisi.setEditable(true);
-        lbl_grupName.setText(anasayfa.groupName);
+        grup_mesaj_akisi.setEditable(true);
+        //  ThisGrupSohbet.lbl_grupName.setText(anasayfa.groupName);
+        Thread.sleep(500);
+        String[] parts = s.split("_");
+        grup_adi = parts[0];
+        kisiler = parts[1];
+        Thread.sleep(100);
 
-//
-       
-//        Thread.sleep(100);
-//        ThisGrupSohbet.grup_adi_.setText(grup_adi);
-//
-//        Message msg = new Message(Message.Message_Type.grupKisiBul);
-//        msg.content = grup_adi + "_" + kisiler;
-//        Thread.sleep(100);
-//        client.Client.Send(msg);
+        ThisGrupSohbet.lbl_grupName.setText(grup_adi);
+
+        Message msg = new Message(Message.Message_Type.groupUsers);
+        msg.content = grup_adi + "_" + kisiler;
+
+        client.Client.Send(msg);
+
     }
 
-    public Group(String grup_ad, String kisi) throws InterruptedException {
+    public Group(String s, int i) throws InterruptedException {
         initComponents();
         ThisGrupSohbet = this;
-        System.out.println("group çalıtı");
-        ThisGrupSohbet.lbl_grupName.setText(anasayfa.groupName);
-//        Message msg = new Message(Message.Message_Type.grupKisiBul);
-//        msg.content = grup_adi + "-" + kisiler;
-//        Thread.sleep(100);
-//        client.Client.Send(msg);
+        grup_mesaj_akisi.setEditable(true);
+        // ThisGrupSohbet.lbl_grupName.setText(anasayfa.groupName);   
+        Thread.sleep(500);
 
+        String[] parts3 = s.split("_");
+        grup_adi = parts3[0];
+        kisiler = parts3[1];
+        ThisGrupSohbet.lbl_grupName.setText(grup_adi);
         Thread.sleep(100);
     }
 
@@ -84,6 +87,8 @@ public class Group extends javax.swing.JFrame {
         jScrollPane2 = new javax.swing.JScrollPane();
         grup_mesaj_akisi = new javax.swing.JTextPane();
         lbl_grupName = new javax.swing.JLabel();
+        btn_back = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setMinimumSize(new java.awt.Dimension(450, 640));
@@ -92,7 +97,7 @@ public class Group extends javax.swing.JFrame {
         grup_mesaj_kutusu.setFont(new java.awt.Font("Courier New", 0, 14)); // NOI18N
         grup_mesaj_kutusu.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(51, 153, 255)));
         getContentPane().add(grup_mesaj_kutusu);
-        grup_mesaj_kutusu.setBounds(70, 410, 310, 60);
+        grup_mesaj_kutusu.setBounds(20, 410, 320, 60);
 
         btn_mesaj_gonder_grup.setText("Send");
         btn_mesaj_gonder_grup.addActionListener(new java.awt.event.ActionListener() {
@@ -101,41 +106,50 @@ public class Group extends javax.swing.JFrame {
             }
         });
         getContentPane().add(btn_mesaj_gonder_grup);
-        btn_mesaj_gonder_grup.setBounds(380, 410, 70, 60);
+        btn_mesaj_gonder_grup.setBounds(20, 480, 430, 60);
 
-        btn_dosya.setText("File");
+        btn_dosya.setText("Choose File");
         btn_dosya.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 btn_dosyaActionPerformed(evt);
             }
         });
         getContentPane().add(btn_dosya);
-        btn_dosya.setBounds(10, 410, 60, 60);
+        btn_dosya.setBounds(350, 410, 100, 60);
 
         grup_mesaj_akisi.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 102, 255)));
         grup_mesaj_akisi.setFont(new java.awt.Font("Courier New", 0, 14)); // NOI18N
         jScrollPane2.setViewportView(grup_mesaj_akisi);
 
         getContentPane().add(jScrollPane2);
-        jScrollPane2.setBounds(10, 60, 440, 340);
+        jScrollPane2.setBounds(20, 60, 430, 340);
         getContentPane().add(lbl_grupName);
-        lbl_grupName.setBounds(70, 20, 250, 40);
+        lbl_grupName.setBounds(120, 10, 330, 40);
+
+        btn_back.setText("Back");
+        btn_back.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btn_backActionPerformed(evt);
+            }
+        });
+        getContentPane().add(btn_back);
+        btn_back.setBounds(30, 10, 80, 40);
+
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/image.jpg"))); // NOI18N
+        getContentPane().add(jLabel1);
+        jLabel1.setBounds(0, 0, 480, 550);
 
         pack();
     }// </editor-fold>                        
 
     private void btn_mesaj_gonder_grupActionPerformed(java.awt.event.ActionEvent evt) {                                                      
-        // TODO add your handling code here:
-        //eger dosya gonderme durumu yoksa clientin yazdigi mesaji mesaj akisina ekler 
-        //tum mesaj akisini karsi clientlara gonderir
+
         if (fileToSend == null) {
-            String ad = client.Client.client_name;
-            String n_ad = ad.toUpperCase();
-            String icerik = n_ad + ":\n" + grup_mesaj_kutusu.getText().toString() + "\n";
-            grup_mesaj_akisi.setText(grup_mesaj_akisi.getText() + icerik);
+
+            grup_mesaj_akisi.setText(grup_mesaj_akisi.getText() + application.anasayfa.user1 + ":\n" + grup_mesaj_kutusu.getText().toString() + "\n");
             grup_mesaj_kutusu.setText("");
 
-            Message msg = new Message(Message.Message_Type.icerikGrup);
+            Message msg = new Message(Message.Message_Type.GroupFileSender);
             msg.content = kisiler + "_" + grup_mesaj_akisi.getText();
             try {
                 Thread.sleep(300);
@@ -143,18 +157,14 @@ public class Group extends javax.swing.JFrame {
                 Logger.getLogger(Group.class.getName()).log(Level.SEVERE, null, ex);
             }
             client.Client.Send(msg);
+            grup_mesaj_kutusu.setText("");
             //dosya gonderme durumu varsa
         } else if (fileToSend != null) {
             try {
-                //dosya gonderildi bilgisini mesaj akisina ekler dosya adiyla
-                //dosyayi karsiya gonderir
-                String ad = client.Client.client_name;
-                String n_ad = ad.toUpperCase();
-                String icerik = n_ad + ":\n" + "--- Dosya : " + fileToSend.getName() + " ---\n" + grup_mesaj_kutusu.getText().toString() + "\n";
-                grup_mesaj_akisi.setText(grup_mesaj_akisi.getText() + icerik);
+                grup_mesaj_akisi.setText(grup_mesaj_akisi.getText() + application.anasayfa.user1 + ":\n" + "--- Dosya : " + fileToSend.getName() + " ---\n" + grup_mesaj_kutusu.getText() + "\n");
                 grup_mesaj_kutusu.setText("");
 
-                Message msg = new Message(Message.Message_Type.icerikGrup);
+                Message msg = new Message(Message.Message_Type.GroupFileSender);
                 msg.content = kisiler + "_" + grup_mesaj_akisi.getText();
                 Thread.sleep(100);
                 try {
@@ -172,7 +182,7 @@ public class Group extends javax.swing.JFrame {
                 fileToSend = null;
 
                 String fileContentBytes_string = Base64.getEncoder().encodeToString(fileContentBytes);
-                Message msg4 = new Message(Message.Message_Type.dosya1);
+                Message msg4 = new Message(Message.Message_Type.File);
                 msg4.content = kisiler + "&" + fileName + "_" + fileContentBytes_string;
                 client.Client.Send(msg4);
                 Thread.sleep(300);
@@ -196,10 +206,19 @@ public class Group extends javax.swing.JFrame {
         if (choose_File == JFileChooser.APPROVE_OPTION) {
             fileToSend = fileChooser.getSelectedFile();
 
-        }else{
+        } else {
             JOptionPane.showMessageDialog(fileChooser, "Try again");
         }
     }                                         
+
+    private void btn_backActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        ThisGrupSohbet.setVisible(false);
+        anasayfa.ThisAnasayfaPage.setVisible(true);
+
+        Message msg = new Message(Message.Message_Type.Back);
+        msg.content = lbl_grupName.getText();
+        client.Client.Send(msg);
+    }                                        
 
     /**
      * @param args the command line arguments
@@ -237,10 +256,12 @@ public class Group extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify                     
+    private javax.swing.JButton btn_back;
     private javax.swing.JButton btn_dosya;
     private javax.swing.JButton btn_mesaj_gonder_grup;
     public javax.swing.JTextPane grup_mesaj_akisi;
     private javax.swing.JTextField grup_mesaj_kutusu;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel lbl_grupName;
     // End of variables declaration                   
