e01bd4fb4b33a40d732e720093cbd994f2cf1682
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2022-05-19 00:05:09 +0300

    Update Client.java

--- client_project/Client.java
+++ client_project/Client.java
@@ -7,7 +7,7 @@ package client;
 
 /**
  *
- * @author
+ * @author gayeu
  */
 import static client.Client.sInput;
 import java.io.File;
@@ -17,114 +17,71 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.util.Base64;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.swing.DefaultListModel;
 import javax.swing.JFileChooser;
-import message.Message;
-import static message.Room.ThisSohbet;
-import message.anasayfa;
+import application.Message;
+import static application.Room.ThisSohbet;
+import application.anasayfa;
 
-/**
- *
- * @author gayeu
- */
+
 class Listen extends Thread {
 
     public void run() {
-        //soket bağlı olduğu sürece dön
         while (Client.socket.isConnected()) {
             try {
-                //mesaj gelmesini blocking olarak dinleyen komut
                 Message received = (Message) (sInput.readObject());
-                //mesaj gelirse bu satıra geçer
-                //mesaj tipine göre yapılacak işlemi ayır.
                 switch (received.type) {
-                    case Connected:
-                        //cevrimici kullanicilar listesini alir ve anasayfada gunceller
-                        anasayfa.ThisAnasayfa.list_users.setModel((DefaultListModel) received.content);
+                    case UserName:
+                        application.anasayfa.user1 = received.content.toString();
                         break;
 
-                    case Name:
-                        //Client'ın name bilgisini alir     
-                        Client.client_name = received.content.toString();
+                    case UserList:
+                        anasayfa.ThisAnasayfaPage.list_users.setModel((DefaultListModel) received.content);
                         break;
 
-                    case icerik:
-                        //mesaj icerigini alir 
-                        String mesajlasilan;
-                        String mesajakisi;
-                        String[] parts = received.content.toString().split("_");
-                        mesajlasilan = parts[0];
-                        mesajakisi = parts[1];
-                        Thread.sleep(100);
-                        message.anasayfa.ThisAnasayfa.setVisible(false);
-                        new message.Room(mesajlasilan).setVisible(true);
-                        message.Room.ThisSohbet.list_mess.setText(mesajakisi);
-                        break;
-
-                    case icerik2:
-                        //mesaj icerigini alir
-                        String mesajlasilan2;
-                        String mesajakisi2;
+                    case Mess:
                         String[] parts2 = received.content.toString().split("_");
-                        mesajakisi2 = parts2[1];
                         Thread.sleep(100);
-                        message.Room.ThisSohbet.setVisible(true);
-                        message.Room.ThisSohbet.list_mess.setText(mesajakisi2);
+                        application.Room.ThisSohbet.setVisible(true);
+                        application.Room.ThisSohbet.list_mess.setText(parts2[1]);
                         break;
 
-                    case baglantiKopar:
-                        //karsi client sohbet kutusunu kapatirsa burada da mesajlasma sonlandirilir 
+                    case Back:
                         ThisSohbet.setVisible(false);
-                        anasayfa.ThisAnasayfa.setVisible(true);
+                        anasayfa.ThisAnasayfaPage.setVisible(true);
                         Thread.sleep(100);
                         break;
 
-                    case grupUsers:
-                        //gruptaki kisiler bilgisi gonderilir grup uyelerine
-                        Thread.sleep(100);
-                       //    grupOlustur.ThisGrupOlustur.users.setModel((DefaultListModel) received.content);
+                    case groupUsers:
+                        application.anasayfa.ThisAnasayfaPage.setVisible(false);
+                        new application.Group(received.content.toString(), 1).setVisible(true);
                         break;
 
-                    case grupKisiBul:
-                        //grup olusturulduktan sonra her clientin anasayfasi kapatilir ve grup sohbet penceresi baslatilir
-                        if (message.anasayfa.ThisAnasayfa.isVisible()) {
-                            message.anasayfa.ThisAnasayfa.setVisible(false);
-                        }
-                        
-                        new message.Group(received.content.toString()).setVisible(true);
-                        break;
-
-                    case kisiBul:
-                        //bireysel mesajlasmada biri sohbeti baslatinca digerinin de sayfasinda o sohbet penceresi acilir
-                        if (message.anasayfa.ThisAnasayfa.isVisible()) {
-                            message.anasayfa.ThisAnasayfa.setVisible(false);
-                        }
+                    case ChatPrivate:
+                        application.anasayfa.ThisAnasayfaPage.setVisible(false);
                         String[] dizi = received.content.toString().split("-");
                         String kisi_ad = dizi[0];
                         Client.kisi_ad2 = dizi[1];
-                        System.out.println("ikinic" + Client.kisi_ad2);
-
-                        new message.Room().setVisible(true);
+                        new application.Room().setVisible(true);
                         break;
 
-                    case icerikGrup:
-                        //gruptaki mesaj iceriklerini gunceller
-                        message.Room.ThisSohbet.list_mess.setText(received.content.toString());
+                    case GroupFileSender:
+                        application.Group.ThisGrupSohbet.grup_mesaj_akisi.setText(received.content.toString());
                         break;
 
-                    case dosya1:
-                        //yollanan dosyayi getirir ve nereye kaydedilecegini sorar
+                    case PrivateFileSender:
+                        application.Room.ThisSohbet.list_mess.setText(received.content.toString());
+                        break;
+
+                    case File:
                         String[] parts4 = received.content.toString().split("_");
                         String fileName = parts4[0];
                         String Content = parts4[1];
-
                         byte[] content_decode = Base64.getDecoder().decode(Content);
-
                         JFileChooser ch = new JFileChooser();
-                        ch.setCurrentDirectory(new File("C:\\Users\\busra\\Desktop\\ağlar_proje_2\\dosya_alma"));
+                        ch.setCurrentDirectory(new File("C:\\Users\\gayeu\\Desktop"));
                         int c = ch.showSaveDialog(null);
                         if (c == JFileChooser.APPROVE_OPTION) {
                             FileOutputStream out = new FileOutputStream(ch.getSelectedFile());
@@ -135,9 +92,7 @@ class Listen extends Thread {
                 }
             } catch (IOException ex) {
                 Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-                //Client.Stop();
-            } //Client.Stop();
-            catch (InterruptedException ex) {
+            } catch (InterruptedException ex) {
                 Logger.getLogger(Listen.class.getName()).log(Level.SEVERE, null, ex);
             } catch (ClassNotFoundException ex) {
                 Logger.getLogger(Listen.class.getName()).log(Level.SEVERE, null, ex);
@@ -149,32 +104,22 @@ class Listen extends Thread {
 public class Client {
 
     public static String kisi_ad2;
-    //her clientın bir soketi olmalı
     public static Socket socket;
-
-    //verileri almak için gerekli nesne
     public static ObjectInputStream sInput;
-    //verileri göndermek için gerekli nesne
     public static ObjectOutputStream sOutput;
-    //serverı dinleme thredi 
     public static Listen listenMe;
-    public static String client_name;
 
     public static void Start(String ip, int port) {
         try {
-            // Client Soket nesnesi
             Client.socket = new Socket(ip, port);
-            Client.Display("Servera bağlandı");
-            // input stream
+            System.out.println("Coonect to server");
             Client.sInput = new ObjectInputStream(Client.socket.getInputStream());
-            // output stream
             Client.sOutput = new ObjectOutputStream(Client.socket.getOutputStream());
             Client.listenMe = new Listen();
             Client.listenMe.start();
 
-            //ilk mesaj olarak isim gönderiyorum
-            Message msg = new Message(Message.Message_Type.Name);
-            msg.content = message.anasayfa.ThisAnasayfa.txt_name.getText();
+            Message msg = new Message(Message.Message_Type.UserName);
+            msg.content = application.anasayfa.ThisAnasayfaPage.txt_name.getText();
             Client.Send(msg);
 
         } catch (IOException ex) {
@@ -182,26 +127,6 @@ public class Client {
         }
     }
 
-    //client durdurma fonksiyonu
-    public static void Stop() {
-        try {
-            if (Client.socket != null) {
-                Client.listenMe.stop();
-                Client.socket.close();
-                Client.sOutput.flush();
-                Client.sOutput.close();
-                Client.sInput.close();
-            }
-        } catch (IOException ex) {
-            Logger.getLogger(Client.class.getName()).log(Level.SEVERE, null, ex);
-        }
-    }
-
-    public static void Display(String msg) {
-        System.out.println(msg);
-    }
-
-    //mesaj gönderme fonksiyonu
     public static void Send(Message msg) {
         try {
             Client.sOutput.writeObject(msg);
