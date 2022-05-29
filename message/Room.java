0a40e16db9b46d2a658ea00eaf9efc315af74642
Gaye Uğur <78750442+gayeugur@users.noreply.github.com>
2022-05-29 19:59:38 +0300

    Update Room.java

--- message/Room.java
+++ message/Room.java
@@ -5,13 +5,12 @@
  */
 package application;
 
+import client.Client;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Base64;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.swing.JFileChooser;
 import javax.swing.JOptionPane;
 
@@ -25,9 +24,9 @@ public class Room extends javax.swing.JFrame {
      * Creates new form sohbet
      */
     public static Room ThisSohbet;
-    public static File file = null;
+    public static File fileName = null;
     public static boolean selectFile = false;
-    public static JFileChooser fileChooser = new JFileChooser();
+    public JFileChooser fileChooser = new JFileChooser();
 
     public Room() {
         initComponents();
@@ -60,7 +59,7 @@ public class Room extends javax.swing.JFrame {
         jScrollPane1 = new javax.swing.JScrollPane();
         list_mess = new javax.swing.JTextArea();
         btn_back = new javax.swing.JButton();
-        btn_dosya = new javax.swing.JButton();
+        btn_sendFile = new javax.swing.JButton();
         jLabel1 = new javax.swing.JLabel();
 
         javax.swing.GroupLayout jDialog1Layout = new javax.swing.GroupLayout(jDialog1.getContentPane());
@@ -95,7 +94,8 @@ public class Room extends javax.swing.JFrame {
         getContentPane().add(txt_mess);
         txt_mess.setBounds(20, 370, 300, 58);
 
-        btn_send.setBackground(new java.awt.Color(255, 255, 255));
+        btn_send.setBackground(new java.awt.Color(204, 204, 255));
+        btn_send.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
         btn_send.setText("Send");
         btn_send.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(51, 102, 255)));
         btn_send.addActionListener(new java.awt.event.ActionListener() {
@@ -106,8 +106,9 @@ public class Room extends javax.swing.JFrame {
         getContentPane().add(btn_send);
         btn_send.setBounds(20, 440, 410, 58);
 
+        list_mess.setBackground(new java.awt.Color(204, 204, 255));
         list_mess.setColumns(20);
-        list_mess.setFont(new java.awt.Font("Courier New", 0, 14)); // NOI18N
+        list_mess.setFont(new java.awt.Font("Arial", 0, 14)); // NOI18N
         list_mess.setLineWrap(true);
         list_mess.setRows(5);
         list_mess.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(51, 102, 255)));
@@ -116,6 +117,8 @@ public class Room extends javax.swing.JFrame {
         getContentPane().add(jScrollPane1);
         jScrollPane1.setBounds(20, 100, 410, 260);
 
+        btn_back.setBackground(new java.awt.Color(204, 204, 255));
+        btn_back.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
         btn_back.setText("Back");
         btn_back.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -125,65 +128,59 @@ public class Room extends javax.swing.JFrame {
         getContentPane().add(btn_back);
         btn_back.setBounds(20, 40, 110, 50);
 
-        btn_dosya.setText("Choose File");
-        btn_dosya.addActionListener(new java.awt.event.ActionListener() {
+        btn_sendFile.setBackground(new java.awt.Color(204, 204, 255));
+        btn_sendFile.setFont(new java.awt.Font("Tahoma", 1, 13)); // NOI18N
+        btn_sendFile.setText("File");
+        btn_sendFile.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_dosyaActionPerformed(evt);
+                btn_sendFileActionPerformed(evt);
             }
         });
-        getContentPane().add(btn_dosya);
-        btn_dosya.setBounds(330, 370, 100, 60);
+        getContentPane().add(btn_sendFile);
+        btn_sendFile.setBounds(340, 370, 90, 60);
 
         jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/image.jpg"))); // NOI18N
         jLabel1.setPreferredSize(new java.awt.Dimension(1000, 2000));
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(0, 0, 550, 580);
+        jLabel1.setBounds(0, 0, 470, 580);
 
         pack();
     }// </editor-fold>                        
 
     private void btn_sendActionPerformed(java.awt.event.ActionEvent evt) {                                         
         if (selectFile == false) {
-            try {
-                list_mess.setText(list_mess.getText() + application.anasayfa.user1 + ":" + txt_mess.getText() + " \n");
-                Message msg = new Message(Message.Message_Type.Mess);
-                msg.content = lbl_name.getText() + "_" + list_mess.getText();
-                Thread.sleep(300);
-                client.Client.Send(msg);
-                txt_mess.setText("");
-            } catch (InterruptedException ex) {
-                Logger.getLogger(Room.class.getName()).log(Level.SEVERE, null, ex);
-            }
+
+            list_mess.setText(list_mess.getText() + application.anasayfa.user1 + ":" + txt_mess.getText() + " \n");
+            txt_mess.setText("");
+
+            Message msg = new Message(Message.Message_Type.Mess);
+            msg.content = lbl_name.getText() + "_" + list_mess.getText();
+            client.Client.Send(msg);
 
         } else {
+            byte file[] = new byte[(int) fileName.length()];
             try {
-                list_mess.setText(list_mess.getText() + application.anasayfa.user1 + ": " + file.getName() + " File" + " \n");
-                txt_mess.setText("");
-                Message msg = new Message(Message.Message_Type.PrivateFileSender);
-                msg.content = lbl_name.getText() + "_" + list_mess.getText();
+                list_mess.setText(list_mess.getText() + application.anasayfa.user1 + ":" + fileName.getName() + " \n");
 
+                BufferedInputStream bis = new BufferedInputStream(new FileInputStream(fileName));
+                bis.read(file, 0, file.length);
+
+                Message message = new Message(Message.Message_Type.SendFile);
+                message.fileList = file;
+                message.content = fileName.getName();
+                Client.Send(message);
+
+                Message msg = new Message(Message.Message_Type.Mess);
+                msg.content = lbl_name.getText() + "_" + list_mess.getText() + "\n";
                 client.Client.Send(msg);
 
-                Thread.sleep(100);
-                FileInputStream fileInputStream = new FileInputStream(file);
-                byte[] fileContentBytes = new byte[fileInputStream.available()];
-                fileInputStream.read(fileContentBytes);
-                fileInputStream.close();
+                selectFile = false;
 
-                String fileContentBytes_string = Base64.getEncoder().encodeToString(fileContentBytes);
-                Message msg4 = new Message(Message.Message_Type.File);
-                msg4.content = lbl_name.getText() + "&" + file.getName() + "_" + fileContentBytes_string;
-                client.Client.Send(msg4);
-                file = null;
-
-            } catch (FileNotFoundException e) {
-                Logger.getLogger(Room.class.getName()).log(Level.SEVERE, null, e);
-            } catch (IOException e) {
-                Logger.getLogger(Room.class.getName()).log(Level.SEVERE, null, e);
-            } catch (InterruptedException e) {
-                Logger.getLogger(Room.class.getName()).log(Level.SEVERE, null, e);
+            } catch (FileNotFoundException ex) {
+                JOptionPane.showMessageDialog(rootPane, "Error");
+            } catch (IOException ex) {
+                JOptionPane.showMessageDialog(rootPane, "Error");
             }
-
         }
 
     }                                        
@@ -199,22 +196,20 @@ public class Room extends javax.swing.JFrame {
         anasayfa.ThisAnasayfaPage.setVisible(true);
 
         Message msg = new Message(Message.Message_Type.Back);
-        msg.content = lbl_name.getText().toString();
+        msg.content = lbl_name.getText();
         client.Client.Send(msg);
     }                                        
 
-    private void btn_dosyaActionPerformed(java.awt.event.ActionEvent evt) {                                          
+    private void btn_sendFileActionPerformed(java.awt.event.ActionEvent evt) {                                             
 
-        int choose_File = fileChooser.showOpenDialog(this);
-
-        if (choose_File == JFileChooser.APPROVE_OPTION) {
-            file = fileChooser.getSelectedFile();
+        int selection = fileChooser.showOpenDialog(this);
+        if (selection == JFileChooser.APPROVE_OPTION) {
+            fileName = fileChooser.getSelectedFile();
             selectFile = true;
-
-        } else {
-            JOptionPane.showMessageDialog(fileChooser, "Try again");
         }
-    }                                         
+
+
+    }                                            
 
     /**
      * @param args the command line arguments
@@ -253,13 +248,13 @@ public class Room extends javax.swing.JFrame {
 
     // Variables declaration - do not modify                     
     private javax.swing.JButton btn_back;
-    private javax.swing.JButton btn_dosya;
     private javax.swing.JButton btn_send;
+    public javax.swing.JButton btn_sendFile;
     private javax.swing.JDialog jDialog1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JScrollPane jScrollPane1;
-    public javax.swing.JLabel lbl_name;
-    public javax.swing.JTextArea list_mess;
+    public static javax.swing.JLabel lbl_name;
+    public static javax.swing.JTextArea list_mess;
     private javax.swing.JTextField txt_mess;
     // End of variables declaration                   
 }
